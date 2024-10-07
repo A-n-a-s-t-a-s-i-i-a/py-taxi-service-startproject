dd48b3d6ad806f8a4714a750a1e22050c1a8f0fe
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-10-07 14:45:24 +0300

    edit admin.py

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from  taxi.models import Manufacturer, Car, Driver
+
+
+@admin.register(Driver)
+class DriverAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (("Additional Info", {"fields": ("license_number", )}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (("Additional Info", {"fields": ("license_number", )}),)
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_filter = ["manufacturer", ]
+    search_fields = ["model", ]
+
+
+admin.site.register(Manufacturer)
+
--- taxi/models.py
+++ taxi/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    country = models.CharField(max_length=250)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=250)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    class Meta:
+        ordering = ("model", )
+
+    def __str__(self) -> str:
+        return f"Car: {self.manufacturer} - {self.model}"
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=250, unique=True)
+    password = models.CharField(max_length=250)
+
+    class Meta:
+        verbose_name = "Driver"
+        verbose_name_plural = "Drivers"
+
+    def __str__(self) -> str:
+        return f"Driver: {self.license_number}"
